2d61595b5e00261309ba975ec198740f0ebbe11d
popgenomics <camille.roux.1983@gmail.com>
2021-10-03 10:32:40 +0200

    v2

--- bin/submit_simulations_2pop_popGrowth.py
+++ bin/submit_simulations_2pop_popGrowth.py
@@ -78,7 +78,7 @@ if mscommand == "":
 
 #tmp = "mkdir {0}/{1}; ".format(path, sub_dir_sim)
 #tmp += "mkdir {0}/{1}/{2}_{3}; ".format(path, sub_dir_sim, sub_dir_model, iteration)
-tmp = "cp {0}/bpfile {0}/{1}/{2}_{3}; ".format(path, sub_dir_sim, sub_dir_model, iteration)
+tmp = "ln -sfn {0}/bpfile {0}/{1}/{2}_{3}; ".format(path, sub_dir_sim, sub_dir_model, iteration)
 tmp += "cd {0}/{1}/{2}_{3}; ".format(path, sub_dir_sim, sub_dir_model, iteration)
 #tmp += "python {0}/priorgen_2pop_popGrowth.py {1} {2} {3} | {0}/msnsam tbs {4} {5} | pypy {0}/mscalc_2pop_SFS.py {6}".format(binpath, model, nmultilocus, config_yaml, nmultilocus*nlocus, mscommand, outgroup)
 tmp += "python {0}/priorgen_2pop_popGrowth.py {1} {2} {3} | java -jar {0}/msms3.2rc-b163.jar tbs {4} {5} | pypy {0}/mscalc_2pop_SFS.py {6}".format(binpath, model, nmultilocus, config_yaml, nmultilocus*nlocus, mscommand, outgroup)
